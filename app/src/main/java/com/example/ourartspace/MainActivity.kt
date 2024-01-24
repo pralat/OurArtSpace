28a66d8145f12642559590464cac15c9ed313e42
Mark Pralat <mpralat@gmail.com>
2024-01-24 14:19:03 -0500

    Used triples for image, title, author. Next, Previous buttons work.

--- app/src/main/java/com/example/ourartspace/MainActivity.kt
+++ app/src/main/java/com/example/ourartspace/MainActivity.kt
@@ -15,12 +15,17 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -50,14 +55,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun OurArtSpace(modifier:Modifier = Modifier) {
     val images = listOf(
-        R.drawable.mercury,
-        R.drawable.venus,
-        R.drawable.earth,
-        R.drawable.mars,
-        R.drawable.jupiter,
-        R.drawable.saturn,
-        R.drawable.uranus,
-        R.drawable.neptune,
+        Triple(R.drawable.mercury, "Mercury", "NASA"),
+        Triple(R.drawable.venus, "Venus", "NASA"),
+        Triple(R.drawable.earth, "Earth", "NASA"),
+        Triple(R.drawable.mars, "Mars", "NASA"),
+        Triple(R.drawable.jupiter, "Jupiter", "NASA"),
+        Triple(R.drawable.saturn, "Saturn", "NASA"),
+        Triple(R.drawable.uranus, "Uranus", "NASA"),
+        Triple(R.drawable.neptune, "Neptune", "NASA"),
     )
     Column(
         modifier = Modifier
@@ -68,11 +73,15 @@ fun OurArtSpace(modifier:Modifier = Modifier) {
     ) {
 
 //        Image(contentScale = ContentScale.FillWidth, painter = painterResource(id = R.drawable.image01), contentDescription = null)
-        val painter = painterResource(id = R.drawable.uranus)
+//        var index = 6
+        var index by remember { mutableStateOf(6) }
+
+        val (image, title, artist) = images[index]
+        val painter = painterResource(id = image /* R.drawable.uranus */)
 
         Image(
             painter = painter, // = painterResource(id = R.drawable.mercury),
-            contentDescription = null,
+            contentDescription = title,
             contentScale = ContentScale.Fit,
             modifier = Modifier
                 .border(width = 8.dp, color = Color.Red)
@@ -90,8 +99,8 @@ fun OurArtSpace(modifier:Modifier = Modifier) {
 //                .fillMaxSize()
 //                .border(width = 8.dp, color = Color.Red),
         )
-        Text("Artwork Title")
-        Text("Artist")
+        Text(title)
+        Text(artist)
         BoxWithConstraints {
             val maxWidth = maxWidth
 
@@ -111,17 +120,19 @@ fun OurArtSpace(modifier:Modifier = Modifier) {
 //                    modifier = Modifier.width(maxWidth / 4)
 //                ) {Text("Next")}
 //            }
-            Row(horizontalArrangement = Arrangement.spacedBy(40.dp))
-//            modifier = Modifier.fillMaxWidth())
+            Row(horizontalArrangement = Arrangement.spacedBy(40.dp),
+            modifier = Modifier
+                .offset(y = -(6).dp)
+                .align(Alignment.BottomCenter))
             {
 
                 Button(
-                    onClick = {},
+                    onClick = { index = (--index).mod(images.size)},
                     modifier = Modifier.width(maxWidth / 4)
                 ) {Text("Previous")}
                 
                 Button(
-                    onClick = {},
+                    onClick = { index = (++index).mod(images.size)},
                     modifier = Modifier.width(maxWidth / 4)
                 ) {Text("Next")}
             }
